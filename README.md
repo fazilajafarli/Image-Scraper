289cb3bf2ddb7762050b3de9a58f4dc0bcf32a75
fazilajafarli <fezile.ceferli96@gmail.com>
2023-01-16 11:59:08 +0400

    change funtion structure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Image-Scraper
 
-This Python application allows you search term (ex. location) and get image links as the result. It uses Google Custom Search API and Google Search Engine. 
-Firstly, search term, then period should be entered.
+This Python application allows you to search any terms (ex. location) and get image links and titles related to that term. It uses Google Custom Search API and Google Search Engine. 
+API key, search term, then period should be entered. Google does not allow the disclosure of the API key, so the API key is hidden. The results are written in a .csv file.
--- src/script.py
+++ src/script.py
@@ -19,7 +19,6 @@ class Result:
     
 
     def send_request(self, **kwargs):    
-        print("aaaaaaaaaaaaaaaaa")
         query_params = {
             "key": self.APIkey, 
             "cx": SEARCHENGINEID,
@@ -29,47 +28,63 @@ class Result:
             **kwargs,
         }
         
-        print(query_params)
-        page = requests.get(URL, params=query_params).json()         
-        self.get_result(page)
-
-        if not query_params.get('start'):
-            self.get_next_pages(page)
+        page = requests.get(URL, params=query_params).json()   
         
+        try:      
+            totalResults = int(page["queries"]["request"][0]["totalResults"])
+
+            if totalResults > 0:
+                self.get_result(page)
+
+            return page, totalResults
+        except:
+            print("No search result found!")
+
+
             
     def get_result(self, page: dict):
-        print("bbbbbbbbbbbbbbbbbbb")
         items_list = []  
-        for i in page['items']:
+        for i in page.get('items'):
             item = f"{i['title']} ; {i['link']} ;"
             items_list.append(item)
 
-        with open("search_results.csv", "a+") as opened_file:
+        with open("src/search_results.csv", "a+") as opened_file:
             for j in items_list:
                 opened_file.write(f"{j} \n")
             opened_file.write("\n")
+  
 
-
-    def get_next_pages(self, page):
-        totalResults = page["queries"]["request"][0]["totalResults"]
+    def get_next_page(self, page: dict):
         try:
             nextPage = page['queries']['nextPage'][0]['startIndex']
-            print(f"Found {totalResults} results!")
-            x = ceil(int(totalResults) / 10)
-            print(nextPage, "ccccccccccc")
-            for i in range(x):
-                if nextPage < 31:  #change it 
-                    self.send_request(start=nextPage)
-                break
+            return nextPage
         except:
             return None
     
 
 
-APIkey = input("Enter APIkey: ")
-exactTerms = input("Search terms: ")
-dateRestrict = input("Select date for search results: d[number]/w[number]/m[number]/y[number]. ex: \"m2\" means last 2 months. ")
+
+def search():
+    APIkey = input("Enter APIkey: ")
+    exactTerms = input("Search terms: ")
+    dateRestrict = input("Select date for search results: d[number]/w[number]/m[number]/y[number]. ex: \"m2\" means last 2 months. ")
 
 
-result = Result(exactTerms, dateRestrict, APIkey)
-result.send_request()
+    result_obj = Result(exactTerms, dateRestrict, APIkey)
+    try:
+        page, totalResults = result_obj.send_request()  
+        x = ceil(totalResults / 10)
+        for i in range(x):
+            nextPage = result_obj.get_next_page(page)
+            print(f"Next page - {nextPage}")
+            if nextPage and nextPage < 101:  
+                page, totalResults = result_obj.send_request(start=nextPage)
+            else:
+                print("Results are ready!")
+                break
+    except:
+        return None
+        
+
+
+search()
